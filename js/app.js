6849c55a4ff1b54f124f3fdd0926a3b6d5208ca9
JeanCarlos <jmatochepascual@gmail.com>
2024-04-28 23:19:34 -0500

    [FEATURE] mostrar autos en el html

--- js/app.js
+++ js/app.js
@@ -1,3 +1,39 @@
 import { autos } from './db.js';
 
-console.log(autos);
+
+// Variables 
+const marcaInputElement = document.querySelector('#marca');
+const yearInputElement = document.querySelector("#year");
+const precioMinimoInputElement = document.querySelector("#minimo");
+const precioMaximoInputElement = document.querySelector("#maximo");
+const puertasInputElement = document.querySelector("#puertas");
+const transmisionInputElement = document.querySelector("#transmision");
+const colorInputElement = document.querySelector("#color");
+const resultadoElement = document.querySelector("#resultado");
+
+
+
+
+
+// Muestra los autos en el HTML
+const mostrarAutos = (autos) => {
+
+  autos.forEach(auto => {
+
+    const { marca, modelo, year, precio, puertas, color, transmision } = auto;
+    const autoHTML = document.createElement('p');
+
+    autoHTML.textContent = `${marca} ${modelo} - ${year} - ${puertas} Puertas - Transmisión: ${transmision} - Precio: ${precio} - Color: ${color}`;
+
+    resultadoElement.appendChild(autoHTML);
+  });
+}
+
+
+
+// Cargar eventos
+document.addEventListener('DOMContentLoaded', () => {
+
+  mostrarAutos(autos); // Muestra los autos al cargar
+
+});
